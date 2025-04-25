9c19f14c0b679399b2baa88e8b192884a69eb422
mi <46509964+nie312122330@users.noreply.github.com>
2025-04-25 10:12:39 +0800

    初始化

---
+++ fun_ext.go
@@ -0,0 +1,10 @@
+package nlibs
+
+// 匿名函数
+func If3[T any](cond bool, ok, no T) T {
+	if cond {
+		return ok
+	} else {
+		return no
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/niexqc/nlibs
+
+go 1.24
