68d0f65fde555fc341f8593e29370f36e909c5e1
mi <46509964+nie312122330@users.noreply.github.com>
2025-04-29 15:00:54 +0800

    xlsx

--- ntools/xslx_ext.go
+++ ntools/xslx_ext.go
@@ -23,12 +23,29 @@ func XlsxRead(fileName string, sheetName string, dataIdx int) (contents [][]stri
 	return contents, err
 }
 
-func XlsxReadCell(fileName string, sheetName, cell string) (contents string, err error) {
+func XlsxReadCell(fileName string, sheetName, cell string) (valStr string, err error) {
 	f, err := excelize.OpenFile(fileName)
 	if err != nil {
-		return contents, err
+		return valStr, err
 	}
 	defer f.Close()
 	val, err := f.GetCellValue(sheetName, cell)
 	return val, err
+}
+
+func XlsxReadCells(fileName string, sheetName string, cells []string) (vals []string, err error) {
+	f, err := excelize.OpenFile(fileName)
+	if err != nil {
+		return vals, err
+	}
+	defer f.Close()
+	for _, cell := range cells {
+		val, err := f.GetCellValue(sheetName, cell)
+		if err != nil {
+			return vals, err
+		}
+		vals = append(vals, val)
+	}
+
+	return vals, err
 }
