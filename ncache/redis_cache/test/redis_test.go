ce625c0cba181521f697a466e3afb8e93158cf91
mi <46509964+nie312122330@users.noreply.github.com>
2025-05-10 13:59:50 +0800

    测试

--- ncache/redis_cache/test/redis_test.go
+++ ncache/redis_cache/test/redis_test.go
@@ -120,24 +120,15 @@ func TestMutexCreate(t *testing.T) {
 	mutex := rediscache.RedisNewMutex("TestMutexCreate", "1", redisService)
 	if mutex.RedisLock() {
 		defer mutex.RedisReleseLock()
-		//执行逻辑
-		fmt.Println("第1个获取到了")
 	}
 }
 
 func TestMutexCreateWithParams(t *testing.T) {
 	op1 := rediscache.RedisMutexSetDelay(time.Minute)
-	op2 := rediscache.RedisMutexSetDelay(time.Duration(5) * time.Second)
+	op2 := rediscache.RedisMutexSetDelay(time.Duration(500) * time.Millisecond)
 	mutex := rediscache.RedisNewMutex("TestMutexCreateWithParams", "1", redisService, op1, op2)
 	if mutex.RedisLock() {
 		defer mutex.RedisReleseLock()
-		slog.Info("第1个获取到了")
-	}
-	mutex2 := rediscache.RedisNewMutex("TestMutexCreateWithParams", "1", redisService, op1, op2)
-	if mutex2.RedisLock() {
-		defer mutex2.RedisReleseLock()
-		//执行逻辑
-		slog.Info("第2个获取到了")
 	}
 }
 
