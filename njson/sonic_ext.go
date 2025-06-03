6ef0276daa8b13f63058e638950f64db8a5f21f9
mi <46509964+nie312122330@users.noreply.github.com>
2025-06-03 10:48:01 +0800

    333

--- njson/sonic_ext.go
+++ njson/sonic_ext.go
@@ -3,6 +3,7 @@ package njson
 import (
 	"github.com/bytedance/sonic"
 	"github.com/bytedance/sonic/ast"
+	"github.com/niexqc/nlibs/nerror"
 )
 
 type NwNode struct {
@@ -22,45 +23,36 @@ func NewNwNodeByMap(data map[string]any) (*NwNode, error) {
 	return NewNwNodeByJsonStr(jsonStr)
 }
 
-func (s *NwNode) GetString(key string) (string, error) {
-	node := s.Node.Get(key)
-	return node.String()
-}
-
-func (s *NwNode) GetStringByPath(paths ...any) (string, error) {
+func (s *NwNode) GetStringByPath(paths ...any) string {
 	val, err := s.GetByPath(paths...).String()
-	return val, err
+	if nil != err {
+		panic(nerror.NewRunTimeErrorWithError("请检查Json", err))
+	}
+	return val
 }
 
-func (s *NwNode) GetInt64(key string) (int64, error) {
-	val, err := s.Get(key).Int64()
-	return val, err
-}
-
-func (s *NwNode) GetInt64ByPath(paths ...any) (int64, error) {
+func (s *NwNode) GetInt64ByPath(paths ...any) int64 {
 	val, err := s.GetByPath(paths...).Int64()
-	return val, err
+	if nil != err {
+		panic(nerror.NewRunTimeErrorWithError("请检查Json", err))
+	}
+	return val
 }
 
-func (s *NwNode) GetFloat64(key string) (float64, error) {
-	val, err := s.Get(key).Float64()
-	return val, err
-}
-
-func (s *NwNode) GetFloat64ByPath(paths ...any) (float64, error) {
+func (s *NwNode) GetFloat64ByPath(paths ...any) float64 {
 	val, err := s.GetByPath(paths...).Float64()
-	return val, err
+	if nil != err {
+		panic(nerror.NewRunTimeErrorWithError("请检查Json", err))
+	}
+	return val
 }
 
-func (s *NwNode) GetBool(key string) (bool, error) {
-	nd := s.Get(key)
-	val, err := nd.Bool()
-	return val, err
-}
-
-func (s *NwNode) GetBoolByPath(paths ...any) (bool, error) {
+func (s *NwNode) GetBoolByPath(paths ...any) bool {
 	val, err := s.GetByPath(paths...).Bool()
-	return val, err
+	if nil != err {
+		panic(nerror.NewRunTimeErrorWithError("请检查Json", err))
+	}
+	return val
 }
 
 func (s *NwNode) ToString() (string, error) {
